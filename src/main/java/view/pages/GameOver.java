fadb7bfb654d34d5c9dd3b03f00895a616c7818f
unknown <maryam.mehdizadeh1384@gmail.com>
2024-07-23 14:00:00 +0330

    ame overview

--- src/main/java/view/pages/GameOver.java
+++ src/main/java/view/pages/GameOver.java
@@ -8,17 +8,21 @@ import java.awt.*;
 
 public class GameOver extends JFrame {
     Update update;
+    private int h = 40;
+    private int w = 300;
+    private int xLoc = 50;
+    private int yLoc = 10;
     public GameOver(Update update){
         this.update = update;
 
-        setSize(300, 200);
+        setSize(400, 300);
         setLocation(500,100);
         setResizable(false);
         setBackground(Color.black);
         setLayout(null);
         setUndecorated(true);
         setVisible(true);
-        setSize(301,201);
+        setSize(401,301);
         requestFocus();
         setFocusable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,22 +33,38 @@ public class GameOver extends JFrame {
     private void add(){
         Menu.getMenu().setXp(Menu.getMenu().getXp() + update.panel.playerView.getXp());
 
-        JLabel xp = new JLabel(String.valueOf(update.panel.playerView.getXp()));
-        xp.setSize(100,70);
-        xp.setLocation(150,70);
+        JLabel xp = new JLabel("xp:"+ update.panel.playerView.getXp());
+        xp.setSize(w,h);
+        xp.setLocation(xLoc,yLoc + h);
         xp.setBackground(Color.black);
 
+        JLabel bullets = new JLabel("bullets:" + update.dataBase.shoots);
+        bullets.setSize(w,h);
+        bullets.setLocation(xLoc,yLoc + 4*h);
+        bullets.setBackground(Color.black);
+
+        JLabel Sbullets = new JLabel("successful shoots:" + update.dataBase.successShoots );
+        Sbullets.setSize(w,h);
+        Sbullets.setLocation(xLoc,yLoc + 3*h);
+        Sbullets.setBackground(Color.black);
+
+        JLabel kills = new JLabel("kills:" + update.dataBase.killedEnemies);
+        kills.setSize(w,h);
+        kills.setLocation(xLoc,yLoc + 2*h);
+        kills.setBackground(Color.black);
+
+
         JButton button = new JButton("menu");
-        button.setSize(200,50);
+        button.setSize(w,h);
         button.setBackground(Color.gray);
-        button.setLocation(50,140);
+        button.setLocation(xLoc,yLoc + 5*h);
         button.addActionListener(e -> {
             startOver();
         });
 
         JLabel vic = new JLabel("victory!");
-        vic.setLocation(100,10);
-        vic.setSize(100,50);
+        vic.setLocation(xLoc,yLoc);
+        vic.setSize(w,h);
         vic.setBackground(Color.black);
 
         if(update.dataBase.getGamePanelModel().victory){
@@ -52,6 +72,9 @@ public class GameOver extends JFrame {
         }
         add(button);
         add(xp);
+        add(bullets);
+        add(kills);
+        add(Sbullets);
     }
     private void startOver(){
         dispose();
@@ -61,5 +84,6 @@ public class GameOver extends JFrame {
         DataBase.getDataBase().remove();
         Menu.getMenu().setVisible(true);
     }
+
 }
 
